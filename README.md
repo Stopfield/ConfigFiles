9b4e982257e6c8238d1632e302041e5840f8af26
Stopfield <thiagoparacampo@hotmail.com>
2023-08-27 17:41:06 -0300

    Readme and config files added

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Config Files
+Those are files that I use to set up Neovim and Alacritty.
+They're pretty simple and straightforward, I'm just saving for future use.
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,13 @@
+return {
+	"m4xshen/autoclose.nvim",
+	"williamboman/mason.nvim",
+	"ms-jpq/coq_nvim",
+	"ms-jpq/coq.artifacts",
+	"ms-jpq/coq.thirdparty",
+	"neovim/nvim-lspconfig",
+	"williamboman/mason-lspconfig.nvim",
+	"ms-jpq/chadtree",
+	"nvim-tree/nvim-web-devicons",
+	"navarasu/onedark.nvim",
+	"nvim-lualine/lualine.nvim"
+}
